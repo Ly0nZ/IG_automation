a41e01e7d1787db5087486f52c680725ebb2b7dc
Ly0nZ <charleymlyons@gmail.com>
2019-04-02 15:47:52 -0700

    Captured the 'like' and 'next' buttons to store in variables.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let count = 0;
 setInterval(function() {
+    let likes = document.getElementsByClassName('glyphsSpriteHeart__outline__24__grey_9'),
+        nextArrow = document.querySelector('.coreSpriteRightPaginationArrow');
 }, 4000);
