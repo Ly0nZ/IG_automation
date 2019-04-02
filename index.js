0ce445349a19fe91ec72c9a6edd78f5f5ba62d76
Ly0nZ <charleymlyons@gmail.com>
2019-04-02 16:01:00 -0700

    Added documentation to code. Updated the operation frequency to 12 seconds.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-let count = 0;
-setInterval(function() {
+let likeCount = 0; //count tracks number of 'likes' completed
+setInterval(function () {
+    //likes stores the like button for each post
     let likes = document.getElementsByClassName('glyphsSpriteHeart__outline__24__grey_9'),
+        //nextArrow captures the selection to move to the next post
         nextArrow = document.querySelector('.coreSpriteRightPaginationArrow');
     if (likes[1]) {
         likes = likes[1].parentElement;
         likeCount++ , likes.click();
     }
     nextArrow.click();
-    console.log(`Posts liked: ${likeCount}`);
-}, 4000);
+    console.log(`Posts liked: ${likeCount}`); //prints number of 'likes' performed to console
+}, 12000); //sets the frequency to perform function; 12000 = 12 seconds
