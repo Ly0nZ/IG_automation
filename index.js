f3eb31fa7c0be110080e2120e8073382de987862
Ly0nZ <charleymlyons@gmail.com>
2019-04-02 15:56:04 -0700

    Added functionality to click 'like', track the number of likes executed, and the number of posts liked will be printed to console following each execution.

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ let count = 0;
 setInterval(function() {
     let likes = document.getElementsByClassName('glyphsSpriteHeart__outline__24__grey_9'),
         nextArrow = document.querySelector('.coreSpriteRightPaginationArrow');
+    if (likes[1]) {
+        likes = likes[1].parentElement;
+        likeCount++ , likes.click();
+    }
+    nextArrow.click();
+    console.log(`Posts liked: ${likeCount}`);
 }, 4000);
