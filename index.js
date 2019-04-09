a5c8ff7a8831f61fdcd29036e9df0f9c79907ee3
Ly0nZ <charleymlyons@gmail.com>
2019-04-08 22:07:22 -0700

    Adjusted frequency and likeCount parameters to prevent account restriction based on Instagram's activity limitations.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let likeCount = 0; //count tracks number of 'likes' completed
 setInterval(function () {
+    // checks number of actions performed to prevent account from being restricted
+    if (likeCount >= 200) {
+        return;
+    }
     //likes stores the like button for each post
     let likes = document.getElementsByClassName('glyphsSpriteHeart__outline__24__grey_9'),
         //nextArrow captures the selection to move to the next post
@@ -10,4 +14,4 @@ setInterval(function () {
     }
     nextArrow.click();
     console.log(`Posts liked: ${likeCount}`); //prints number of 'likes' performed to console
-}, 12000); //sets the frequency to perform function; 12000 = 12 seconds
+}, 25000); //sets the frequency to perform function; 12000 = 12 seconds
